ffb1bb797049f3a5ec3758e177663dfead3e0948
ZF Chung (Zack) <zfchung1@gmail.com>
2021-09-26 13:29:07 +0800

    Location class returns neightbours()

---
+++ src/Location/index.spec.ts
@@ -0,0 +1,18 @@
+import {Location} from "./index";
+
+
+describe("Testing Location class", () => {
+  it("should return a list of locations that are considered neighbours", () => {
+    // Given
+    const location = new Location(1, 1);
+    // When
+    // Then
+    /*
+    (0,0) (1,0) (2,0)
+    (0,1) (1,1) (2,1)
+    (0,2) (1,2) (2,2)
+    */
+    const expectedResult = [[0, 0], [1, 0], [2, 0], [0, 1], [2, 1], [0, 2], [1, 2], [2, 2]]
+    expect(location.neighbours()).toEqual((expectedResult));
+  })
+})
--- src/Location/index.ts
+++ src/Location/index.ts
@@ -3,10 +3,24 @@ export class Location {
   coordinateY: number;
   coordinate: number[];
 
-  constructor(coordinateX: number, coordinateY: number){
+  constructor(coordinateX: number, coordinateY: number) {
     this.coordinateX = coordinateX;
     this.coordinateY = coordinateY;
     this.coordinate = [this.coordinateX, this.coordinateY];
+  }
+
+  neighbours() {
+    // calculate a list of locations that are considered neighbors
+    let coordinateList: number[][] = [];
+    coordinateList.push([this.coordinateX - 1, this.coordinateY - 1]);
+    coordinateList.push([this.coordinateX, this.coordinateY - 1]);
+    coordinateList.push([this.coordinateX + 1, this.coordinateY - 1]);
+    coordinateList.push([this.coordinateX - 1, this.coordinateY]);
+    coordinateList.push([this.coordinateX + 1, this.coordinateY]);
+    coordinateList.push([this.coordinateX - 1, this.coordinateY + 1]);
+    coordinateList.push([this.coordinateX, this.coordinateY + 1]);
+    coordinateList.push([this.coordinateX + 1, this.coordinateY + 1]);
+    return coordinateList;
   }
 
 }
