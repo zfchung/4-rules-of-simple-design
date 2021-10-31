8c1a37b350725f74164a444eac51697732db94a2
ZF Chung (Zack) <zfchung1@gmail.com>
2021-10-31 18:12:17 +0800

    created LivingCell and DeadCell extends Cell class

--- src/Cell/index.ts
+++ src/Cell/index.ts
@@ -7,19 +7,4 @@ export class Cell {
     this.numberOfNeighbours = 0;
   }
 
-  get aliveInNextGeneration() {
-    if (this.isAlive) {
-      return this.isStableNeighborhood();
-    } else {
-      return this.isGeneticallyFertileNeighbourhood();
-    }
-  }
-
-  private isGeneticallyFertileNeighbourhood() {
-    return this.numberOfNeighbours == 3
-  }
-
-  private isStableNeighborhood() {
-    return this.numberOfNeighbours == 2 || this.numberOfNeighbours == 3;
-  }
 }
--- src/DeadCell/index.ts
+++ src/DeadCell/index.ts
@@ -1,22 +1,19 @@
 import {Location} from "../Location";
 import {Cell} from "../Cell";
 
-export class LivingCell extends Cell {
+export class DeadCell extends Cell {
 
   constructor(public location: Location) {
     // 'super' must be called before accessing 'this' in the constructor of a derived class.
     super();
   }
 
-  public positionAt() {
-    return this.location.coordinate();
-  }
-
   public isAliveInNextGeneration() {
-    return this.isStableNeighborhood();
+    return this.isGeneticallyFertileNeighbourhood();
   }
 
-  private isStableNeighborhood() {
-    return this.numberOfNeighbours == 2 || this.numberOfNeighbours == 3;
+  private isGeneticallyFertileNeighbourhood() {
+    return this.numberOfNeighbours == 3;
   }
+
 }
